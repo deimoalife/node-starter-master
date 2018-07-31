296b489b7c43592a5c23eb04c81d4c6f085104fd
Alexey Golovachev <snow.limur@gmail.com>
2018-08-01 02:29:42 +0300

    добавил пример работы с базой и HTTP методами

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Question = mongoose.model('Question', {text: String});
+
+let router = express.Router();
+
+router.get('/', function (req, res) {
+    Question.find().exec().then(questions => {
+        res.send({status: true, data: questions});
+    }).catch(err => {
+        console.error(err);
+        res.send({status: false, error: 'Internal Server Error'});
+    });
+});
+
+router.post('/', function (req, res) {
+    // @todo validate input
+    if (!req.body) {
+        return res.send({status: false, error: 'Bad request'});
+    }
+
+    if (!req.body.hasOwnProperty('text') || !req.body.text) {
+        return res.send({status: false, error: 'Bad request: text must be nonempty string'});
+    }
+
+    let newQuestion = new Question(req.body);
+    newQuestion.save().then(result => {
+        res.send({status: true, data: result});
+    }).catch(err => {
+        console.error(err);
+        res.send({status: false, error: 'Internal server error'});
+    });
+});
+
+router.delete('/', function (req, res) {
+    // @todo validate input
+    Question.findOneAndDelete({_id: req.body.id}).then(result => {
+        res.send({status: true, data: result});
+    }).catch(err => {
+        console.error(err);
+        res.send({status: false, error: 'Internal server error'});
+    })
 });
 
 module.exports = router;
